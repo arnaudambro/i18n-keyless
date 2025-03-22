59df57be8d44dc2c62597e4ded2cec8371c24b3f
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-03-22 21:36:51 +0100

    better type readme

--- packages/react/I18nKeylessText.tsx
+++ packages/react/I18nKeylessText.tsx
@@ -3,10 +3,32 @@ import { useI18nKeyless } from "./store";
 import { TranslationOptions } from "i18n-keyless-core";
 
 export interface I18nKeylessTextProps {
+  /**
+   * children must be a string
+   * it's the text to translate from your primary language
+   */
   children: string;
+  /**
+   * the keys to replace in the text
+   * it's an object where the key is the placeholder and the value is the replacement
+   * example: { "{{name}}": "John" } will replace all the {{name}} in the text with "John"
+   * RegEx is `key.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"))` so you can use use your own syntax
+   */
   replace?: Record<string, string>;
+  /**
+   * the context of the translation
+   * it's useful for ambiguous translations, like "8 heures" in French could be "8 AM" or "8 hours"
+   */
   context?: string;
+  /**
+   * if true, some helpful logs will be displayed in the console
+   */
   debug?: boolean;
+  /**
+   * if true, the translation will be saved as temporary
+   * it's useful if you want to use the translation in a different context
+   * you can leave it there forever, or remove it once your translation is saved
+   */
   forceTemporary?: TranslationOptions["forceTemporary"];
 }
 

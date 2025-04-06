e89b75b24768402fb5c84e3ddbd5770f2b61ca8f
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-04-06 07:44:59 +0200

    fix imports

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -2,3 +2,18 @@ export { I18nKeylessText } from "./I18nKeylessText";
 export { init, setCurrentLanguage, useCurrentLanguage } from "./store";
 export { clearI18nKeylessStorage } from "./utils";
 export type { I18nKeylessTextProps } from "./I18nKeylessText";
+export {
+  type I18nConfig,
+  type Lang,
+  type PrimaryLang,
+  type Translations,
+  type TranslationStore,
+  type TranslationStoreState,
+  type I18nKeylessRequestBody,
+  type I18nKeylessResponse,
+  type TranslationOptions,
+  getTranslation,
+  fetchAllTranslations,
+  validateLanguage,
+  queue,
+} from "i18n-keyless-core";

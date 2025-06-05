bcaf601cc0dc1eab79940ff53015ec07eaae00fe
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-06-05 10:50:07 +0200

    fix if too many messages

--- packages/core/api.ts
+++ packages/core/api.ts
@@ -1,21 +1,21 @@
 export const api = {
   fetchTranslation: async (url: string, options: RequestInit) => {
     return fetch(url, options)
-      .then((res) => res.json())
+      .then((res) => (res.status === 200 ? res.json() : { ok: false, error: res.statusText }))
       .catch((err) => {
         return { ok: false, error: err.message };
       });
   },
   fetchTranslationsForOneLanguage: async (url: string, options: RequestInit) => {
     return fetch(url, options)
-      .then((res) => res.json())
+      .then((res) => (res.status === 200 ? res.json() : { ok: false, error: res.statusText }))
       .catch((err) => {
         return { ok: false, error: err.message };
       });
   },
   fetchAllTranslationsForAllLanguages: async (url: string, options: RequestInit) => {
     return fetch(url, options)
-      .then((res) => res.json())
+      .then((res) => (res.status === 200 ? res.json() : { ok: false, error: res.statusText }))
       .catch((err) => {
         return { ok: false, error: err.message };
       });

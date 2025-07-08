d6ac301fa554d8aec381bcc6c6ba939d189183f8
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-07-08 12:08:10 +0200

    export useI18nKeyless

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,5 +1,12 @@
 export { I18nKeylessText, I18nKeylessText as T } from "./I18nKeylessText";
-export { init, setCurrentLanguage, useCurrentLanguage, getTranslation, getSupportedLanguages } from "./store";
+export {
+  init,
+  setCurrentLanguage,
+  useCurrentLanguage,
+  getTranslation,
+  getSupportedLanguages,
+  useI18nKeyless,
+} from "./store";
 export { clearI18nKeylessStorage, validateLanguage } from "./utils";
 export type { I18nKeylessTextProps } from "./I18nKeylessText";
 export { type I18nConfig, type TranslationStoreState, type TranslationOptions, type TranslationStore } from "./types";
